9d13ddb5e3a2a6a528c36a827600b39a0b5f9c12
samihttps <148577681+samihttps@users.noreply.github.com>
2023-12-02 10:31:28 -0800

    variables,strings & numbers

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+    const conferenceTickets = 50
+    var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking applicationconferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets,"are still available.")
+	fmt.Println("Get your tickets here to attend")
+   
+    
+}
